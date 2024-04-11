8721e9e1a4d1a4fcd181a2c071a23eb007114c22
kazeyanagi <kyana0817@gmail.com>
2024-04-11 22:19:04 +0900

    :tada: try loading config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use serde_yaml;
+use std::fs;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    text: String,
 }
+
+fn main() {
+    let file = fs::read_to_string("./config.yaml").expect("cannot read file");
+    let config = serde_yaml::from_str::<Config>(&file).expect("cannot parse yaml");
+
+    println!("{:?}", config)
+}
+
+// use russh::client::Config;
+// use russh_config::parse_home;
+
+// fn main() {
+//     let config = parse_home("oravpn").unwrap();
+//     println!("{:?}", config);
+// }
