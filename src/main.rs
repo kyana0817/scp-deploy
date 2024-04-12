fa687decf4444f923a2f2dae8f89538a510b1bef
kazeyanagi <kyana0817@gmail.com>
2024-04-12 23:19:24 +0900

    :fire: unnecessary comment

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,3 @@ fn main() {
     let config = parse_config(None);
     println!("{:?}", config)
 }
-
-// use russh::client::Config;
-// use russh_config::parse_home;
-
-// fn main() {
-//     let config = parse_home("oravpn").unwrap();
-//     println!("{:?}", config);
-// }
