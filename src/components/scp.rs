5c9bbe29316b0be767b4075ca8cee8065a431af6
kazeyanagi <kyana0817@gmail.com>
2024-04-17 23:36:53 +0900

    :sparkles: add scp ready

--- src/components/scp.rs
+++ src/components/scp.rs
@@ -1,23 +1,73 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use std::path::PathBuf;
+use std::sync::Arc;
+
 use crate::loader::ScpConfig;
-use russh_config::Config;
+use russh::client;
+use russh_config;
+use russh_keys::{key, load_secret_key};
 
 #[derive(Debug)]
 pub struct Scp {
-    pub ssh: Config,
+    pub ssh_config: russh_config::Config,
+    pub client_config: client::Config,
+}
+
+struct Client {}
+
+#[async_trait]
+impl client::Handler for Client {
+    type Error = russh::Error;
+
+    async fn check_server_key(
+        &mut self,
+        _server_key: &key::PublicKey,
+    ) -> Result<bool, Self::Error> {
+        Ok(true)
+    }
 }
 
 impl Scp {
     pub fn new(config: ScpConfig) -> Self {
-        let ssh = match config {
+        let ssh_config = match config {
             ScpConfig {
                 alias: Some(alias), ..
-            } => Config::default(&alias),
+            } => russh_config::Config::default(&alias),
             ScpConfig {
                 host: Some(host), ..
-            } => Config::default(&host),
+            } => russh_config::Config::default(&host),
             _ => panic!("Invalid scp config schema"),
         };
-        // todo
-        Scp { ssh }
+
+        Scp {
+            ssh_config,
+            client_config: client::Config::default(),
+        }
+    }
+
+    pub async fn deploy(mut self) -> Result<()> {
+        let config = Arc::new(self.client_config);
+        let sh = Client {};
+        let key_path = PathBuf::from(
+            self.ssh_config
+                .identity_file
+                .clone()
+                .unwrap_or("".to_string()),
+        );
+        let key_pair = load_secret_key(key_path, None)?;
+        let host_name = self.ssh_config.host_name.clone();
+        let port = self.ssh_config.port.clone();
+        let user = self.ssh_config.user.clone();
+        let mut connect = client::connect(config, (host_name, port), sh).await?;
+        let auth_res = connect
+            .authenticate_publickey(user, Arc::new(key_pair))
+            .await?;
+
+        if !auth_res {
+            anyhow::bail!("Authentication failed");
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-mod loader;
-use loader::parse_config;
 mod components;
+mod loader;
 use components::Scp;
+use loader::parse_config;
 fn main() {
     let config = parse_config(None);
     let scp = Scp::new(config.scp.unwrap());
